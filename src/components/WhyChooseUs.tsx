8bc4d84dd90f3ccd00fb7d35e2652a1d6b0fdd49
grooveyman <edegraftamaning85@gmail.com>
2026-05-26 12:06:39 +0000

    update to landing page

--- src/components/WhyChooseUs.tsx
+++ src/components/WhyChooseUs.tsx
@@ -8,30 +8,37 @@ const WhyChooseUs: React.FC = () => {
                     <div className="col-md-3 why-choose-cover">
                         <div className="why-choose-items d-flex justify-content-start gap-1">
                             <div className="why-choose-icon"></div>
-                            <div>Easy Online Banking</div>
+                            <div>Quality Assurance</div>
                         </div>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni facere enim, aspernatur odio tenetur voluptate, fuga sequi ab totam itaque, s</p>
+                        <p>All our equipment and vehicles are regularly maintained to ensure safety and efficiency.</p>
                     </div>
                     <div className="col-md-3 why-choose-cover">
                         <div className="why-choose-items d-flex justify-content-start gap-1">
                             <div className="why-choose-icon"></div>
-                            <div>Easy Online Banking</div>
+                            <div>Reliable Service</div>
                         </div>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni facere enim, aspernatur odio tenetur voluptate, fuga sequi ab totam itaque, s</p>
+                        <p>We are committed to delivering dependable rental solutions you can trust.</p>
                     </div>
                     <div className="col-md-3 why-choose-cover">
                         <div className="why-choose-items d-flex justify-content-start gap-1">
                             <div className="why-choose-icon"></div>
-                            <div>Easy Online Banking</div>
+                            <div>Affordable Pricing</div>
                         </div>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni facere enim, aspernatur odio tenetur voluptate, fuga sequi ab totam itaque, s</p>
+                        <p>We offer competitive and flexible pricing packages suitable for different budgets.</p>
                     </div>
                     <div className="col-md-3 why-choose-cover">
                         <div className="why-choose-items d-flex justify-content-start gap-1">
                             <div className="why-choose-icon"></div>
-                            <div>Easy Online Banking</div>
+                            <div>Professional Team</div>
                         </div>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni facere enim, aspernatur odio tenetur voluptate, fuga sequi ab totam itaque, s</p>
+                        <p>Our experienced staff provides excellent customer support and professional guidance.</p>
+                    </div>
+                    <div className="col-md-3 why-choose-cover">
+                        <div className="why-choose-items d-flex justify-content-start gap-1">
+                            <div className="why-choose-icon"></div>
+                            <div>Customer-Focused Approach</div>
+                        </div>
+                        <p>We prioritize customer satisfaction by understanding and meeting the unique needs of every client.</p>
                     </div>
                 </div>
                 
