63dd289ffde581f67d8373a2ddf6bad70ca13dd5
grooveyman <edegraftamaning85@gmail.com>
2026-05-30 14:22:01 +0000

    update to nav

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,45 +1,116 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "./nav.css";
 
-const NavBar:React.FC = () => {
+const NavBar: React.FC = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light">
             <div className="container">
                 <Link className="navbar-brand" to="/">
-                    <img src={`${import.meta.env.BASE_URL}images/logo.png`} alt="Asab Ghana Logo" width="auto" height="50" className="d-inline-block align-text-top" />
+                    <img
+                        src={`${import.meta.env.BASE_URL}images/logo.png`}
+                        alt="Asab Ghana Logo"
+                        width="auto"
+                        height="50"
+                        className="d-inline-block align-text-top"
+                    />
                 </Link>
 
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse justify-content-start" id="navbarNav">
+
+                <div
+                    className="collapse navbar-collapse justify-content-start"
+                    id="navbarNav"
+                >
                     <ul className="navbar-nav ms-lg-auto navitems">
+
                         <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                            <NavLink
+                                to="/"
+                                end
+                                className={({ isActive }) =>
+                                    `nav-link ${isActive ? "active-nav" : ""}`
+                                }
+                            >
+                                Home
+                            </NavLink>
                         </li>
+
                         <li className="nav-item">
-                            <Link className="nav-link" to="/book">Book</Link>
+                            <NavLink
+                                to="/book"
+                                className={({ isActive }) =>
+                                    `nav-link ${isActive ? "active-nav" : ""}`
+                                }
+                            >
+                                Book
+                            </NavLink>
                         </li>
+
                         <li className="nav-item">
-                            <Link to="fleet" className="nav-link">Fleet</Link>
+                            <NavLink
+                                to="/fleet"
+                                className={({ isActive }) =>
+                                    `nav-link ${isActive ? "active-nav" : ""}`
+                                }
+                            >
+                                Fleet
+                            </NavLink>
                         </li>
+
                         <li className="nav-item">
-                            <Link className="nav-link" to="/services">Services</Link>
+                            <NavLink
+                                to="/services"
+                                className={({ isActive }) =>
+                                    `nav-link ${isActive ? "active-nav" : ""}`
+                                }
+                            >
+                                Services
+                            </NavLink>
                         </li>
+
                         <li className="nav-item">
-                            <Link className="nav-link" to="/about">About Us</Link>
+                            <NavLink
+                                to="/about"
+                                className={({ isActive }) =>
+                                    `nav-link ${isActive ? "active-nav" : ""}`
+                                }
+                            >
+                                About Us
+                            </NavLink>
                         </li>
+
                         <li className="nav-item">
-                            <Link className="nav-link" to="/about/#contact">Contact</Link>
+                            <Link className="nav-link" to="/about#contact">
+                                Contact
+                            </Link>
                         </li>
+
                         <li className="nav-item">
-                            <Link className="nav-link" to="/faq">FAQ</Link>
+                            <NavLink
+                                to="/faq"
+                                className={({ isActive }) =>
+                                    `nav-link ${isActive ? "active-nav" : ""}`
+                                }
+                            >
+                                FAQ
+                            </NavLink>
                         </li>
+
                     </ul>
                 </div>
             </div>
         </nav>
     );
-}
+};
 
 export default NavBar;
