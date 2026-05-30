d77b68a0b21a7551f2b96b285c15cf5880c16433
grooveyman <edegraftamaning85@gmail.com>
2026-05-30 13:58:44 +0000

    update to home

--- src/components/HeroBook.tsx
+++ src/components/HeroBook.tsx
@@ -1,42 +1,61 @@
+import { useState } from "react";
+import DatePicker from "react-datepicker";
 
 
 const HeroBook: React.FC = () => {
+    const [pickupDate, setPickupDate] = useState<Date | null>(null);
+    const handlePickupDate = (date: Date | null) => {
+        setPickupDate(date);
+    }
+    const [returnDate, setReturnDate] = useState<Date | null>(null);
+    const handleReturnDate = (date: Date | null) => {
+        setReturnDate(date);
+    }
+
+    const [availableOnly, setAvailableOnly] = useState(false);
+
+
     return (
         <>
             <div className="book-form">
 
                 <form className="book-form-body">
                     <div className="mb-2">
-                        <input type="text" className="form-control" id="pickup" placeholder="Pickup Address" />
-                    </div>
-                    <div className="mb-2">
-                        <input type="text" className="form-control" id="dropoff" placeholder="Drop-off Address" />
-                    </div>
-                    <div className="mb-2">
-                        <input type="date" className="form-control" id="dateTime" />
-                    </div>
-                    <div className="mb-2 d-flex align-items-center gap-2">
-
-                        <label
-                            htmlFor="pickupTime"
-                            className="form-label mb-0"
-                            style={{ whiteSpace: "nowrap" }}
-                        >
-                            Pickup Time:
-                        </label>
-
-                        <input
-                            type="text"
+                        <DatePicker
+                            selected={pickupDate}
+                            onChange={handlePickupDate}
+                            showTimeSelect
+                            dateFormat="MMMM d, yyyy h:mm aa"
                             className="form-control"
-                            id="pickupTime"
-                            placeholder="10:30"
-                            style={{ maxWidth: "120px" }}
-                        />
+                            placeholderText="Pickup Date and Time"
+                            minDate={new Date()} />
+                    </div>
+                    <div className="mb-2">
+                        <DatePicker
+                            selected={returnDate}
+                            onChange={handleReturnDate}
+                            showTimeSelect
+                            dateFormat="MMMM d, yyyy h:mm aa"
+                            className="form-control"
+                            placeholderText="Return Date and Time"
+                            minDate={new Date()} />
+                    </div>
 
-                        <select className="form-control" style={{ maxWidth: "70px" }}>
-                            <option>AM</option>
-                            <option>PM</option>
-                        </select>
+                    <div className="mb-2 d-flex align-items-start gap-2 flex-column flex-wrap">
+
+                        <div>
+                            <label>Do you want your rented car delivered? </label>
+                        </div>
+                        <div>
+                            <label className="toggle-switch">
+                                <input
+                                    type="checkbox"
+                                    checked={availableOnly}
+                                    onChange={() => setAvailableOnly(!availableOnly)}
+                                />
+                                <span className="slider"></span>
+                            </label>
+                        </div>
 
                     </div>
                     <button type="submit" className="btn btn-primary">Book Now</button>
